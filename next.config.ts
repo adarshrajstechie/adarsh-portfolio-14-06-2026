995310aab7dd2052b3815cbbe90c67e7d7c4f7af
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2026-06-14 22:37:04 +0530

    today

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  devIndicators: false,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
